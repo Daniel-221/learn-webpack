9e668ecc2f0e24d0e72cd04aaa020bdeb1036c6a
zhaozilin <18150407431@163.com>
2024-07-04 17:59:58 +0800

    pack

---
+++ src/index.js
@@ -0,0 +1,4 @@
+'use strict';
+var component = require('./components/testComponent/index.js');
+
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
 module.exports = {
   // 指定构建模式，有两个参数可选
   // 开发阶段使用development,因为打包速度快
   // 上线阶段使用production,因为项目上线需要文件体积小
   mode: 'development',
-  entry: "./demo01",
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + '/dist',
+    filename: 'main.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
 }
