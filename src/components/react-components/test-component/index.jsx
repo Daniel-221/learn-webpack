7bec044a7850f9cf18771bf9de4dc6e6d5d8d1dd
zhaozilin <18150407431@163.com>
2024-07-05 18:03:24 +0800

    learn sourseMap .etc

--- src/components/react-components/test-component/index.jsx
+++ src/components/react-components/test-component/index.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import './index.css'
 import './index.less'
+import image from './assets/poi-pic.png'
+
 
 export default class index extends Component {
   render() {
+    const c = a.b
     return (
       <div className="root-container">
         this is a react component
         <text className='red'>red</text>
+        <img className='image' src={image} />
       </div>
     )
   }
--- webpack.config.js
+++ webpack.config.js
@@ -5,16 +5,26 @@ module.exports = {
   // 开发阶段使用development,因为打包速度快
   // 上线阶段使用production,因为项目上线需要文件体积小
   mode: 'development',
+  resolve: {
+    alias: {
+      //告诉 webpack ,程序员写的代码中，@符号表示 src 这一层目录
+      '@': path.join(__dirname, '/src/')
+    }
+  },
   entry: "./src/index.js",
   output: {
     path: __dirname + '/dist',
-    filename: 'main.js'
+    filename: 'main.js',
+    assetModuleFilename: 'images/[hash][ext][query]'
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
     })
   ],
+  //sourceMap就是一个信息文件，里面存储这位置信息。也就是说， Source Map文件中存储着压缩混淆后的代码，所对应的转换前的位置。
+  //保证运行时报错的行数与源代码的行数保持一致
+  devtool: 'eval-source-map',
   devServer: {
     //初次打包后自动打开浏览器
     open: true
@@ -47,7 +57,12 @@ module.exports = {
         use: [
           'style-loader', 'css-loader', 'less-loader'
         ]
-      }
+      },
+      // 从 Webpack 5 开始，file-loader 和 url-loader 已经被 asset module type 替代，提供了一种更简单的方式来处理图片和其他静态资源
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset', // 自动选择 between 'asset/resource' 和 'asset/inline'
+      },
     ]
   },
   resolve: {
